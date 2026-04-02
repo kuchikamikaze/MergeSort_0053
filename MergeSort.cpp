a6f27e631f2fb28da806271c512287318c39418a
Naufal Arif <na713634@gmail.com>
2026-04-02 18:54:59 +0800

    fungsi input

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -3,3 +3,25 @@ using namespace std;
 
 int arr[20], B[20];
 int n;
+
+void input() {
+    while (true) {
+        cout << "Masukkan Panjang element array : ";
+        cin >> n;
+
+        if (n <= 20) {
+            break;
+        } else {
+            cout << "\nMaksimal panjang array adalah 20" << endl;
+        }
+    }
+
+    cout << "\n-----------------------" << endl;
+    cout << "Inputkan Isi elemen array" << endl;
+    cout << "-----------------------" << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << "Array index ke-" << i << " : ";
+        cin >> arr[i];
+    }
+}
