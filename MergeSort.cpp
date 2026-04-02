6055c1783415ab1d72d759dd934641e7c6b2f565
Naufal Arif <na713634@gmail.com>
2026-04-02 19:00:44 +0800

    memindahkan array

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -50,3 +50,20 @@ void mergeSort(int low, int high) {
         }
         k++;
     }
+
+    while (j <= high) {
+        B[k] = arr[j];
+        j++;
+        k++;
+    }
+
+    while (i <= mid) {
+        B[k] = arr[i];
+        i++;
+        k++;
+    }
+
+    for (int x = low; x <= high; x++) {
+        arr[x] = B[x];
+    }
+}
